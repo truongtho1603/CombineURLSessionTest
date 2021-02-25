6e069432355ec1e353e930f486e061e9457c6601
Tho Do <tho.do@medtronic.com>
2021-02-25 08:59:23 +0000

    Fix two libraries

--- Package.swift
+++ Package.swift
@@ -15,10 +15,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CombineURLSessionExtension",
-            targets: ["CombineURLSessionExtension"]),
-        .library(
-            name: "CombineURLSessionExtensionTest",
-            targets: ["CombineURLSessionExtensionTest"]),
+            targets: ["CombineURLSessionExtension", "CombineURLSessionExtensionTest"])
     ],
     dependencies: [
     ],
